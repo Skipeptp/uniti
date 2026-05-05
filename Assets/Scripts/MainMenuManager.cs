bbe56a70f4b82985b0a3003e50316ed618bce1fd
Skipeptp <glebkarpuhin8@gmail.com>
2026-05-05 12:02:23 +0300

    Описание что изменил

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,12 +1,13 @@
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class MainMenuManager : MonoBehaviour
 {
     [Header("Панели")]
-    public GameObject instructionPanel;   // Панель с инструкцией
-    public GameObject resultsPanel;       // Панель с результатами
+    public GameObject mainPanel;
+    public GameObject instructionPanel;
+    public GameObject resultsPanel;
 
     [Header("Текст результатов")]
     public TextMeshProUGUI lastCoinsText;
@@ -14,64 +15,39 @@ public class MainMenuManager : MonoBehaviour
 
     private void Start()
     {
-        // Скрываем панели при старте
+        if (mainPanel != null) mainPanel.SetActive(true);
         if (instructionPanel != null) instructionPanel.SetActive(false);
         if (resultsPanel != null) resultsPanel.SetActive(false);
     }
 
-    // Кнопка "Старт"
     public void OnStartButton()
     {
-        if (GameManager.Instance != null)
-            GameManager.Instance.StartGame();
-        else
-        {
-            // Если GameManager не найден, грузим сцену напрямую
-            UnityEngine.SceneManagement.SceneManager.LoadScene("0");
-        }
+        SceneManager.LoadScene("0");
     }
 
-    // Кнопка "Результаты"
     public void OnResultsButton()
     {
-        // Закрываем инструкцию если открыта
+        if (lastCoinsText != null)
+            lastCoinsText.text = "Последний забег: " + PlayerPrefs.GetInt("LastCoins", 0) + " монет";
+        if (recordText != null)
+            recordText.text = "Рекорд: " + PlayerPrefs.GetInt("RecordCoins", 0) + " монет";
+
+        if (mainPanel != null) mainPanel.SetActive(false);
         if (instructionPanel != null) instructionPanel.SetActive(false);
-
-        // Обновляем текст
-        if (GameManager.Instance != null)
-        {
-            if (lastCoinsText != null)
-                lastCoinsText.text = "Последний забег: " + GameManager.Instance.GetLastCoins() + " монет";
-            if (recordText != null)
-                recordText.text = "Рекорд: " + GameManager.Instance.GetRecord() + " монет";
-        }
-        else
-        {
-            // Читаем напрямую из PlayerPrefs если GameManager не существует
-            if (lastCoinsText != null)
-                lastCoinsText.text = "Последний забег: " + PlayerPrefs.GetInt("LastCoins", 0) + " монет";
-            if (recordText != null)
-                recordText.text = "Рекорд: " + PlayerPrefs.GetInt("RecordCoins", 0) + " монет";
-        }
-
-        // Показываем панель результатов
         if (resultsPanel != null) resultsPanel.SetActive(true);
     }
 
-    // Кнопка "Инструкция"
     public void OnInstructionButton()
     {
-        // Закрываем результаты если открыты
+        if (mainPanel != null) mainPanel.SetActive(false);
         if (resultsPanel != null) resultsPanel.SetActive(false);
-
-        if (instructionPanel != null)
-            instructionPanel.SetActive(true);
+        if (instructionPanel != null) instructionPanel.SetActive(true);
     }
 
-    // Кнопка "Закрыть" на любой панели
     public void OnClosePanel()
     {
         if (instructionPanel != null) instructionPanel.SetActive(false);
         if (resultsPanel != null) resultsPanel.SetActive(false);
+        if (mainPanel != null) mainPanel.SetActive(true);
     }
 }
